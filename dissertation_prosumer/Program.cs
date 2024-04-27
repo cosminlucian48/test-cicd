e9a5970319aecd08300cf3c1308d29425de6f3b7
Cosmin Lucian Bejan Topse <cbejan1908@gmail.com>
2024-04-27 20:59:29 +0300

    my first commit

---
+++ dissertation_prosumer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dissertation_prosumer
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var env = new ActressMas.EnvironmentMas(0, 1500);
+
+            var prosumerAgent1 = new ProsumerAgent();
+            var prosumerAgent2 = new ProsumerAgent();
+            var prosumerAgent3 = new ProsumerAgent();
+            env.Add(prosumerAgent1, "prosumer1");
+            env.Add(prosumerAgent2, "prosumer2");
+            env.Add(prosumerAgent3, "prosumer3");
+
+            env.Start();
+        }
+    }
+}
---
+++ dissertation_prosumer/Utils.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dissertation_prosumer
+{
+    public class Utils
+    {
+        public static int NoTurns = 10;
+
+        public static int Delay = 1000;
+        public static Random RandNoGen = new Random();
+
+        public static void ParseMessage(string content, out string action, out string parameters)
+        {
+            string[] t = content.Split();
+
+            action = t[0];
+
+            parameters = "";
+
+            if (t.Length > 1)
+            {
+                for (int i = 1; i < t.Length - 1; i++)
+                    parameters += t[i] + " ";
+                parameters += t[t.Length - 1];
+            }
+        }
+
+        public static string Str(object p1, object p2)
+        {
+            return string.Format("{0} {1}", p1, p2);
+        }
+    }
+}
